506def03070d18f64d2a3c6c4971a7667a0cf33d
Yuyu <Your.Name1@gmx.de>
2015-11-23 15:28:44 +0100

    Integrated moment and moment-timezone

--- web/init.js
+++ web/init.js
@@ -5,12 +5,12 @@
  *  http://chinachu.akkar.in/
 **/
 (function _init_chinachu() {
-	
+
 	"use strict";
-	
+
 	var PARAM = window.location.search.replace('?', '').toQueryParams();
 	window.location.query = PARAM;
-	
+
 	var DEBUG = (PARAM.debug === 'on');
 	if ((typeof window.console !== 'object') || (DEBUG === false)) {
 		var console = {
@@ -22,9 +22,9 @@
 	} else {
 		var console = window.console;
 	}
-	
+
 	console.info('[welcome]', 'initializing application.');
-	
+
 	//
 	// 設定
 	//
@@ -32,31 +32,35 @@
 	var INDEX_PATH   = './page/';
 	var LOCALES      = ['ja'];
 	var LOCALES_PATH = './locales/';
-	
+
 	var API_ROOT = PARAM.api || '/api/';
 	console.info('API_ROOT:', API_ROOT);
-	
+
 	var COLOUR = PARAM.colour || localStorage.getItem('colour') || null;
-	
+
 	//
 	// 必要なライブラリ等
 	//
 	var REQUIRES = [
 		/* socket.io */
 		'./socket.io/socket.io.js',
-		
+
 		/* hypers */
 		'./lib/hyperform.js',
 		'./lib/hyperform.css',
-		
+
 		/* date.format */
 		'./lib/date.format.js',
-		
+
+		/* moment, moment-timezone */
+		'./lib/moment/min/moment.min.js',
+		'./lib/moment-timezone/builds/moment-timezone-with-data.min.js',
+
 		/* chinachu */
 		'./chinachu.css',
 		'./class.js'
 	];
-	
+
 	// _|_
 	// \ /
 	//  v
@@ -65,9 +69,9 @@
 		// アプリケーションオブジェクト
 		//
 		var app = {};
-		
+
 		if (DEBUG) window.app = app;//デバッグ用
-		
+
 		app.view = {};
 		app.stat = {};
 		app.env  = {};
@@ -75,9 +79,9 @@
 		app.f    = {};
 		app.def  = {};
 		app.timer= {};
-		
+
 		app.socket = null;
-		
+
 		app.chinachu = {
 			status   : {},
 			rules    : [],
@@ -86,16 +90,16 @@
 			recording: [],
 			recorded : []
 		}
-		
-		
+
+
 		// global
 		window.global = {
 			chinachu: app.chinachu
 		};
-		
+
 		// misc
 		app.def.apiRoot = API_ROOT;
-		
+
 		app.def.colors  = [
 			'#6495ed',
 			'#f39700',
@@ -105,7 +109,7 @@
 			'#ec6d71',
 			'#1e50a2'
 		];
-		
+
 		app.def.categoryColor = {
 			anime      : '#fcbde1',
 			information: '#bdfce8',
@@ -117,18 +121,18 @@
 			cinema     : '#d6bdfc',
 			etc        : '#eeeeee'
 		};
-		
+
 		//
 		// 画面の初期化
 		//
 		app.view.body = new sakura.ui.Body().clear();
-		
+
 		//
 		// ローディングマスク
 		//
 		app.view.loadingMask = new sakura.ui.Container({className: 'fullmask loading'}).render(app.view.body);
 		app.view.panicMask   = new sakura.ui.Container({className: 'fullmask panic'}).render(app.view.body).hide();
-		
+
 		//
 		// 多言語化
 		//
@@ -136,9 +140,9 @@
 			locales    : LOCALES,
 			localesPath: LOCALES_PATH
 		});
-		
+
 		var __ = localization.convert;
-		
+
 		Object.extend(String.prototype, {
 			__: function(keywords){
 				if (keywords) {
@@ -148,20 +152,20 @@
 				}
 			}
 		});
-		
+
 		//
 		// ベースカラー変更
 		//
 		if (COLOUR) {
 			var colour = '#' + COLOUR;
-			
+
 			app.view.body.entity.insert(
 				new Element('style').insert(
 					'body { background: ' + colour + '; }'
 				)
 			);
 		}
-		
+
 		//
 		// メイン読み込み
 		//
@@ -169,9 +173,9 @@
 			// ページマネージャー
 			app.pm = new sakura.page.Manager({
 				app: app,
-				
+
 				indexPath: INDEX_PATH,
-				
+
 				onReady: function _onReadyPM() {
 					// main.js
 					new sakura.util.Loader({
@@ -183,23 +187,23 @@
 				}
 			});//<--pm
 		};//<--loadMain
-		
+
 		if (isLoadedRequires) {
 			loadMain();
 		} else {
 			// JSとCSSの読み込み
 			new sakura.util.Requires(REQUIRES, function _main() {
 				console.log('[application]', 'initializing done.');
-				
+
 				loadMain();
-				
+
 				isLoadedRequires = true;
 			});
 		}
 	}//<--init
-	
+
 	var isLoadedRequires = false;
-	
+
 	Event.observe(window, 'load', init, false);
 
 })();
