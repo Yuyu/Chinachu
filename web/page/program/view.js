265e477fc98f3fbb49233134384248e65cdc0567
Yuyu <Your.Name1@gmx.de>
2015-11-24 09:37:52 +0100

    Fixed MM to mm in time format

--- web/page/program/view.js
+++ web/page/program/view.js
@@ -266,7 +266,7 @@ P = Class.create(P, {
 
 		var meta = new flagrate.Element('div', { 'class': 'program-meta' }).update(
 			' &ndash; ' +
-			moment(program.end).tz('Asia/Tokyo').format('HH:MM') +
+			moment(program.end).tz('Asia/Tokyo').format('HH:mm') +
 			' (' + (program.seconds / 60) + '分間)<br>' +
 			'<small><span class="bg-cat-' + program.category + '">' + program.category + '</span> / ' + program.channel.type + ': ' +
 			'<a href="#!/search/top/skip=1&chid=' + program.channel.id + '/">' + program.channel.name + '</a>' +
