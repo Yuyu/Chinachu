c1ad3f10b6859ef949860ebc59d0ebbd79093192
Yuyu <Yuyu>
2015-11-25 13:12:52 +0100

    Added localization and eng translation for notifications

--- web/chinachu.js
+++ web/chinachu.js
@@ -1,24 +1,24 @@
 (function _chinachu() {
-	
+
 	"use strict";
-	
+
 	console.log('----', 'launched', '----');
-	
+
 	// notify
 	app.notify = new flagrate.Notify({
 		title  : 'Chinachu',
 		hMargin: (10 + 25)
 	});
-	
+
 	// apiクライアントの初期化
 	app.api = new chinachu.api.Client({
 		apiRoot: app.def.apiRoot
 	});
-	
+
 	// エラートラップ
 	window.onerror = function _errorTrap(mes, file, num) {
 		console.error('[!] ERROR:', mes, file, num);
-		
+
 		if (!DEBUG) {
 			app.notify.create({
 				title  : 'ERROR: ' + file + ':' + num,
@@ -30,68 +30,68 @@
 			});
 		}
 	};
-	
+
 	app.socket = io.connect(window.location.protocol + '//' + window.location.host, {
 		connectTimeout: 3000,
 		resource: window.location.pathname.replace(/^\/|[^\/]*$/g, '') + 'socket.io',
 	});
-	
+
 	// コントロールビュー初期化
 	app.f.enterControlView = function _enterControlView() {
 		console.log('entering to control view');
-		
+
 		// クリーンアップ
 		$$('.no-ctrl', '.header', '.middle', '.footer').each(function(ele) {
 			ele.remove();
 		});
-		
+
 		// リセット
 		document.stopObserving('sakurapanel:pm:reload');
 		document.stopObserving('sakurapanel:pm:load');
 		document.stopObserving('sakurapanel:pm:complete');
-		
+
 		app.pm.category = 'status';
-		
+
 		document.observe('sakurapanel:pm:reload', function() {
 			app.pm.disableHashControl();
-			
+
 			app.pm.title.update('...');
 			document.title = '...';
-			
+
 			document.fire('chinachu:reload');
 		});
-		
+
 		document.observe('sakurapanel:pm:unload', function() {
 			app.view.sideBody.removeAll();
 		});
-		
+
 		document.observe('sakurapanel:pm:load', function() {
 			if (app.pm.pageData.nohead) {
 				app.view.main.entity.addClassName('nohead');
 			} else {
 				app.view.main.entity.removeClassName('nohead');
 			}
-			
+
 			if (app.pm.pageData.background) {
 				app.view.mainBody.entity.style.background = app.pm.pageData.background;
 			} else {
 				app.view.mainBody.entity.style.background = '';
 			}
-			
+
 			app.pm.title.update(app.pm.title.innerHTML.__());
 			document.title = 'Chinachu: ' + [app.pm.page.__(), app.pm.category.__()].join(' - ');
-			
+
 			app.view.header.all().each(function(a) { a.unselect(); });
 			try { app.view.header.one(app.pm.category).select(); } catch (e) {}
-			
+
 			app.view.sideBody.removeAll();
-			
+
 			if (app.pm.index.category[app.pm.category].pageIndex) {
 				app.view.middle.entity.removeClassName('noside');
-				
+
 				app.pm.index.category[app.pm.category].pageIndex.each(function(pageName)  {
 					var page = app.pm.index.category[app.pm.category].page[pageName];
-					
+
 					app.view.sideBody.add({
 						key: 'page-index-' + pageName,
 						ui : new sakura.ui.Button({
@@ -102,15 +102,15 @@
 							}
 						})
 					});//<--app.view.sideBody.add
-					
+
 					if (pageName === app.pm.page) {
 						app.view.sideBody.one('page-index-' + pageName).select();
-						
+
 						if (app.pm.pageData.background) {
 							app.view.sideBody.one('page-index-' + pageName).entity.style.boxShadow = 'inset -2px 0 0 ' + app.pm.pageData.background;
 						}
 					}
-					
+
 					new sakura.ui.Tooltip({
 						target: app.view.sideBody.one('page-index-' + pageName).entity,
 						html  : page.label.__()
@@ -120,14 +120,14 @@
 				app.view.middle.entity.addClassName('noside');
 			}
 		});//<--observe sakurapanel:pm:load
-		
+
 		document.observe('sakurapanel:pm:complete', function() {
 			app.pm.enableHashControl();
 		});
-		
+
 		// location.hashによるロケーション制御を有効にする
 		app.pm.enableHashControl(true);
-		
+
 		// 構造体
 		app.view.header   = new sakura.ui.Headbar().render(app.view.body);
 		app.view.middle   = new sakura.ui.Container({ className: 'middle' }).render(app.view.body);
@@ -140,7 +140,7 @@
 		app.view.toolbar  = new sakura.ui.Container({ className: 'main-head-toolbar' }).render(app.view.mainHead).insert(app.pm.toolbar);
 		app.view.mainBody = new sakura.ui.Container({ className: 'main-body' }).render(app.view.main).insert(app.pm.content);
 		app.view.footer   = new sakura.ui.Navbar({ className: 'footer' }).render(app.view.body);
-		
+
 		app.view.header.add({
 			key: 'reload',
 			ui : new sakura.ui.Button({
@@ -152,7 +152,7 @@
 				}
 			})
 		});
-		
+
 		app.pm.index.categoryIndex.each(function(categoryName, i) {
 			var button = new sakura.ui.Button({
 				className: 'button-dent category-' + categoryName,
@@ -162,29 +162,29 @@
 					window.location.hash = '!/' + categoryName + '/' + app.pm.index.category[categoryName].defaultPage + '/';
 				}
 			});
-			
+
 			app.view.header.add({
 				key: categoryName,
 				ui : button
 			});
-			
+
 			if (['rules', 'reserves', 'recording', 'recorded'].indexOf(categoryName) !== -1) {
 				app.view.header.one(categoryName).badge = new sakura.ui.Element({
 					tagName: 'span'
 				}).render(app.view.header.one(categoryName).entity);
 			}
 		});
-		
+
 		//
 		app.view.middle.entity.addClassName('extend');
-		
+
 		if (!Prototype.Browser.IE) {
 			app.pm.content.stopObserving('scroll');
 			app.pm.content.observe('scroll', app.f.contentOnScroll);
 		}
 		clearInterval(app.timer.intervalOverline);
 		app.timer.intervalOverline = setInterval(app.f.contentOnScroll, 1000);
-		
+
 		app.view.footer.add({
 			key: 'chinachu',
 			ui : new sakura.ui.Button({
@@ -202,7 +202,7 @@
 				}
 			})
 		});
-		
+
 		app.view.footer.add({
 			key: 'operator-status',
 			ui : new sakura.ui.Button({
@@ -211,7 +211,7 @@
 				icon : './icons/status-offline.png',
 			})
 		});
-		
+
 		app.view.footer.add({
 			key: 'wui-status',
 			ui : new sakura.ui.Button({
@@ -221,7 +221,7 @@
 			})
 		});
 	};
-	
+
 	// オーバーラインハンドラ
 	app.f.contentOnScroll = function _contentOnScroll() {
 		if (app.pm.content.scrollTop > 0) {
@@ -229,14 +229,14 @@
 		} else {
 			app.view.mainHead.entity.removeClassName('overline');
 		}
-		
+
 		if (app.pm.content.scrollHeight - app.pm.content.offsetHeight > app.pm.content.scrollTop) {
 			app.view.mainBody.entity.addClassName('overline');
 		} else {
 			app.view.mainBody.entity.removeClassName('overline');
 		}
 	};
-	
+
 	app.f.getProgramById = function _getProgramById(id) {
 		for (var i = 0; i < app.chinachu.recording.length; i++) {
 			if ((app.chinachu.recording[i].id === id) && (app.chinachu.recording[i].pid)) {
@@ -244,21 +244,21 @@
 				return app.chinachu.recording[i];
 			}
 		}
-		
+
 		for (var i = 0; i < app.chinachu.recorded.length; i++) {
 			if (app.chinachu.recorded[i].id === id) {
 				app.chinachu.recorded[i]._isRecorded = true;
 				return app.chinachu.recorded[i];
 			}
 		}
-		
+
 		for (var i = 0; i < app.chinachu.reserves.length; i++) {
 			if (app.chinachu.reserves[i].id === id) {
 				app.chinachu.reserves[i]._isReserves = true;
 				return app.chinachu.reserves[i];
 			}
 		}
-		
+
 		for (var i = 0; i < app.chinachu.schedule.length; i++) {
 			for (var j = 0; j < app.chinachu.schedule[i].programs.length; j++) {
 				if (app.chinachu.schedule[i].programs[j].id === id) {
@@ -266,34 +266,34 @@
 				}
 			}
 		}
-		
+
 		return null;
 	};
-	
+
 	var socketOnConnect = function _socketOnConnect() {
 		app.view.loadingMask.hide();
-		
+
 		document.fire('chinachu:connect');
-		
+
 		app.view.footer.one('wui-status').entity.style.backgroundImage = 'url(./icons/status.png)';
-		
+
 		//app.notify.create({ title: 'Chinachu', message: 'CONNECTED'.__() });
 	};
-	
+
 	var socketOnDisconnect = function _socketOnDisconnect() {
 		app.view.loadingMask.show();
-		
+
 		document.fire('chinachu:disconnect');
-		
+
 		app.view.footer.one('wui-status').entity.style.backgroundImage = 'url(./icons/status-offline.png)';
-		
+
 		app.notify.create({ title: 'Chinachu', message: 'DISCONNECTED'.__() });
 	};
-	
+
 	var socketOnStatus = function _socketOnStatus(data) {
 		app.chinachu.status = data;
 		document.fire('chinachu:status', app.chinachu.status);
-		
+
 		if (app.view.footer.one('operator-status')._status !== data.operator.alive) {
 			if (data.operator.alive) {
 				app.view.footer.one('operator-status').entity.style.backgroundImage = 'url(./icons/status.png)';
@@ -302,7 +302,7 @@
 			}
 		}
 		app.view.footer.one('operator-status')._status = data.operator.alive;
-		
+
 		if (app.view.footer.one('count') === null) {
 			app.view.footer.add({
 				key: 'count',
@@ -315,12 +315,12 @@
 		}
 		app.view.footer.one('count').entity.update(data.connectedCount);
 	};
-	
+
 	var socketOnRules = function _socketOnRules(data) {
 		app.chinachu.rules = data;
 		document.fire('chinachu:rules', app.chinachu.rules);
 	};
-	
+
 	var socketOnNotifyRules = function () {
 		new Ajax.Request('./api/rules.json', {
 			method: 'get',
@@ -330,11 +330,11 @@
 			}
 		});
 	};
-	
+
 	document.observe('chinachu:rules', function (e) {
 		app.view.header.one('rules').badge.update(e.memo.length.toString(10));
 	});
-	
+
 	var socketOnReserves = function _socketOnReserves(data) {
 		var dt = new Date().getTime();
 		data.each(function(program, i) {
@@ -343,17 +343,17 @@
 			}
 		});
 		data = data.compact();
-		
+
 		app.chinachu.reserves = data;
 		document.fire('chinachu:reserves', app.chinachu.reserves);
 	};
-	
+
 	var socketOnNotifyReserves = function () {
 		new Ajax.Request('./api/reserves.json', {
 			method: 'get',
 			onSuccess: function (t) {
 				var data = t.responseJSON;
-				
+
 				var dt = new Date().getTime();
 				data.each(function(program, i) {
 					if (program.start - dt < 1000 * 60) {
@@ -361,22 +361,22 @@
 					}
 				});
 				data = data.compact();
-				
+
 				app.chinachu.reserves = data;
 				document.fire('chinachu:reserves', app.chinachu.reserves);
 			}
 		});
 	};
-	
+
 	document.observe('chinachu:reserves', function (e) {
 		app.view.header.one('reserves').badge.update(e.memo.length.toString(10));
 	});
-	
+
 	var socketOnSchedule = function _socketOnSchedule(data) {
 		app.chinachu.schedule = data;
 		document.fire('chinachu:schedule', app.chinachu.schedule);
 	};
-	
+
 	var socketOnNotifySchedule = function () {
 		new Ajax.Request('./api/schedule.json', {
 			method: 'get',
@@ -386,12 +386,12 @@
 			}
 		});
 	};
-	
+
 	var socketOnRecording = function _socketOnRecording(data) {
 		app.chinachu.recording = data;
 		document.fire('chinachu:recording', app.chinachu.recording);
 	};
-	
+
 	var socketOnNotifyRecording = function () {
 		new Ajax.Request('./api/recording.json', {
 			method: 'get',
@@ -401,20 +401,20 @@
 			}
 		});
 	};
-	
+
 	document.observe('chinachu:recording', function (e) {
 		app.view.header.one('recording').badge.update(e.memo.length.toString(10));
-		
+
 		if (e.memo.length === 0) {
 			$('favicon').href = './favicon.ico';
 		} else {
 			$('favicon').href = './favicon-active.ico';
 		}
-		
+
 		if (app.stat.lastRecordingCount) {
 			if (app.stat.lastRecordingCount < e.memo.length) {
 				app.notify.create({
-					text   : '録画開始: ' + e.memo.last().title,
+					text   : 'RECORDING STARTED'.__() + ': ' + e.memo.last().title,
 					timeout: 10,
 					onClick: function() {
 						window.location.hash = '!/program/view/id=' + e.memo.last().id + '/';
@@ -423,17 +423,17 @@
 			}
 		}
 		app.stat.lastRecordingCount = e.memo.length;
-		
+
 		setTimeout(socketOnNotifyReserves, 0);
 	});
-	
+
 	var socketOnRecorded = function _socketOnRecorded(data) {
 		data = data.reverse();
-		
+
 		app.chinachu.recorded = data;
 		document.fire('chinachu:recorded', app.chinachu.recorded);
 	};
-	
+
 	var socketOnNotifyRecorded = function () {
 		new Ajax.Request('./api/recorded.json', {
 			method: 'get',
@@ -443,14 +443,14 @@
 			}
 		});
 	};
-	
+
 	document.observe('chinachu:recorded', function (e) {
 		app.view.header.one('recorded').badge.update(e.memo.length.toString(10));
-		
+
 		if (app.stat.lastRecordedCount) {
 			if (app.stat.lastRecordedCount < e.memo.length) {
 				app.notify.create({
-					text   : '録画終了: ' + e.memo.first().title,
+					text   : 'RECORDING FINISHED'.__() + ': ' + e.memo.first().title,
 					timeout: 10,
 					onClick: function() {
 						window.location.hash = '!/program/view/id=' + e.memo.first().id + '/';
@@ -460,57 +460,57 @@
 		}
 		app.stat.lastRecordedCount = e.memo.length;
 	});
-	
+
 	app.socket.on('connect'   , socketOnConnect);
 	app.socket.on('disconnect', socketOnDisconnect);
-	
+
 	app.socket.on('status'    , socketOnStatus);
-	
+
 	app.socket.on('notify-rules'    , socketOnNotifyRules);
 	app.socket.on('notify-reserves' , socketOnNotifyReserves);
 	app.socket.on('notify-recording', socketOnNotifyRecording);
 	app.socket.on('notify-recorded' , socketOnNotifyRecorded);
 	app.socket.on('notify-schedule' , socketOnNotifySchedule);
-	
+
 	// ジャンル通知
 	var remindedProgramIds = [];
 	var notifiedProgramIds = [];
 	setInterval(function () {
-		
+
 		if (!localStorage.getItem('schedule.notify.categories')) {
 			return;
 		}
-		
+
 		var categories = JSON.stringify(localStorage.getItem('schedule.notify.categories') || '[]');
-		
+
 		if (categories.length === 0 || !app.chinachu.schedule || app.chinachu.schedule.length === 0) {
 			return;
 		}
-		
+
 		var programs = app.chinachu.schedule.pluck('programs').flatten();
-		
+
 		var i, l = programs.length, t = Date.now();
 		for (i = 0; l > i; i++) {
 			if (categories.indexOf(programs[i].category) === -1 || t > programs[i].end) {
 				continue;
 			}
-			
+
 			var text = programs[i].title + ' (' + programs[i].channel.name + ') - ';
-			
+
 			if (programs[i].start - t < 80000 && programs[i].start - t > 50000 && remindedProgramIds.indexOf(programs[i].id) === -1) {
 				remindedProgramIds.push(programs[i].id);
 
-				text = 'まもなく: ' + text;
+				text = 'SHORTLY'.__() + ': ' + text;
 			} else if (programs[i].start - t < 3000 && programs[i].start - t > -10000 && notifiedProgramIds.indexOf(programs[i].id) === -1) {
 				notifiedProgramIds.push(programs[i].id);
 
-				text = '放送開始: ' + text;
+				text = 'BROADCAST STARTED'.__() + ': ' + text;
 			} else {
 				continue;
 			}
-			
+
 			text += chinachu.dateToString(new Date(programs[i].start));
-			
+
 			app.notify.create({
 				text   : text,
 				timeout: 10,
@@ -520,8 +520,8 @@
 			});
 		}
 	}, 10000);
-	
+
 	// go
 	app.f.enterControlView();
-	
+
 })();
